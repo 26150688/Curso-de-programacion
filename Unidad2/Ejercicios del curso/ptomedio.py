3036a7e9c286a8a7ab12cf95e03277982f380f66
Cesar <26150688@aduascalientes.tecnm.mx>
2026-08-17 01:27:32 -0600

    Agrega Programa para el promedio de tres numeros

---
+++ Unidad2/Ejercicios del curso/ptomedio.py
@@ -0,0 +1,6 @@
+# Programa para el promedio de tres números
+n1 = float(input("Ingrese el primer número: "))
+n2 = float(input("Ingrese el segundo número: "))
+n3 = float(input("Ingrese el tercer número: "))
+promedio = (n1 + n2 + n3) / 3
+print(f"El promedio es: {promedio}")
