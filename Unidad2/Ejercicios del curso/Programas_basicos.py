fb932cd2dfd2fa57cd6057bfe951dcad02424efa
Cesar <26150688@aduascalientes.tecnm.mx>
2026-08-16 23:57:35 -0600

    Agrega programa de area de rectangulo

---
+++ Unidad2/Ejercicios del curso/Programas_basicos.py
@@ -0,0 +1,5 @@
+#Programa calculo de Área de un rectángulo
+base = float(input("Ingrese la base: "))
+altura = float(input("Ingrese la altura: "))
+area = base * altura
+print(f"El área del rectángulo es: {area}")
